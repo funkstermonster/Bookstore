4599b6e17a1bc0608f02d2d34241ced0cfc3ac6f
Fanter <fanni.szasz96@gmail.com>
2021-07-16 11:04:33 +0200

    added requestmapping annotation

--- src/main/java/hu/fanter/bookstore/controller/BookController.java
+++ src/main/java/hu/fanter/bookstore/controller/BookController.java
@@ -4,12 +4,14 @@ import hu.fanter.bookstore.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BookController {
     @Autowired
     private BookService service;
 
+    @RequestMapping("/book-list")
     public String bookList(Model model) {
         model.addAttribute("books", service.findAll());
 
